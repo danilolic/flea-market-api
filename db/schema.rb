58e5b7fbad8739c0d07d7ebd7d65034cbe6ad4b4
Danilo de Lima <danilolicd@gmail.com>
2019-10-27 20:30:02 -0300

    add price to listing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_22_011016) do
+ActiveRecord::Schema.define(version: 2019_10_27_232306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,11 @@ ActiveRecord::Schema.define(version: 2019_10_22_011016) do
   create_table "listings", force: :cascade do |t|
     t.string "title"
     t.string "description"
-    t.integer "kind"
+    t.integer "kind", default: 0
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price", default: 0
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
