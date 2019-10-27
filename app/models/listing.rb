6768baec3cc5a3e1f47ff3c7ab6a062e053df8f5
Danilo de Lima <danilolicd@gmail.com>
2019-10-27 20:31:28 -0300

    add validates to listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,4 +4,9 @@ class Listing < ApplicationRecord
   belongs_to :user
 
   enum status: %i[sell lend exchange]
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :kind, presence: true
+  validates :user, presence: true
 end
