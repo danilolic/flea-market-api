af1329e4d3a156f737c3e4ee6a8bdee1e14e1f50
Danilo de Lima <danilolicd@gmail.com>
2019-10-27 20:39:45 -0300

    add endpoint to show logged user

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'signup' },
+                     controllers: {
+                       sessions: 'sessions',
+                       registrations: 'registrations'
+                     }
   resources :listings
-    devise_for :users,
-               path: '',
-               path_names: {
-                 sign_in: 'login',
-                 sign_out: 'logout',
-                 registration: 'signup'
-               },
-               controllers: {
-                 sessions: 'sessions',
-                 registrations: 'registrations'
-               }
+  resource :user, only: :show
 end
