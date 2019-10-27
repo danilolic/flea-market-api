649d807556a8bf6dff7dfa73b29988559806dbba
Danilo de Lima <danilolicd@gmail.com>
2019-10-27 20:32:05 -0300

    add dependent :destroy do listings

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
 
-  has_many :listings
+  has_many :listings, dependent: :destroy
 end
