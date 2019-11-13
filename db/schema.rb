afb12cb7366af41dc5f66cb58ac7c46eba1e5353
Danilo de Lima <danilolicd@gmail.com>
2019-11-13 18:33:14 -0300

    add active to listings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_211058) do
+ActiveRecord::Schema.define(version: 2019_11_13_213227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2019_11_04_211058) do
     t.datetime "updated_at", null: false
     t.integer "price", default: 0
     t.string "avatar"
+    t.boolean "active"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
