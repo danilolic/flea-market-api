e29e2706f3633f816f3661bde5c285ddc46e9910
Danilo de Lima <danilolicd@gmail.com>
2019-11-18 12:33:33 -0300

    permit active attribute to listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -54,6 +54,6 @@ class ListingsController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def listing_params
-    params.require(:listing).permit(:title, :description, :kind, :price, :avatar)
+    params.require(:listing).permit(:title, :description, :kind, :price, :avatar, :active)
   end
 end
