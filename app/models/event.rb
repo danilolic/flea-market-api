780d0b52c543908d41c102184f4c36c4d2ac3e82
Danilo de Lima <danilolicd@gmail.com>
2019-11-18 12:59:39 -0300

    add missing dependent to event relationship

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@
 
 class Event < ApplicationRecord
   belongs_to :calendar
-  has_many :event_subjects
+  has_many :event_subjects, dependent: :destroy
   has_many :subjects, through: :event_subjects
 end
